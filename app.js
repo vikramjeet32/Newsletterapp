5f6a3442a49001edef09708267109655ac2e1ca4
Vikramjeet Sharma <81208125+vikramjeet32@users.noreply.github.com>
2022-11-28 10:18:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 
 app.get("/", function(req,res) {
-    res.sendFile(__dirname + "/signup.html");
+    res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/", function(req,res){
